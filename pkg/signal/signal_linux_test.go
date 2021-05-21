13ebbad489113084b431286e033b973c6f4663d8
David Scott <dave@recoil.org>
2021-05-21 15:40:57 +0100

    pkg/signal: run more tests on Darwin
    Previously the file `signal_linux_test.go` had `// +build darwin linux`
    but:
    
    ```
    dave@m1 signal % GO111MODULE=off go test -timeout 30s -run ^TestCatchAllIgnoreSigUrg$
    testing: warning: no tests to run
    PASS
    ok  	github.com/docker/docker/pkg/signal	0.113s
    ```
    
    I think the filename `_linux_test.go` prevented the file being compiled
    on Darwin.
    
    This patch moves tests which work on both Linux and Darwin to
    `signal_linux_darwin_test.go` and removes the conflicting build tags.
    
    Unfortunately `TestCatchAll` does not work on Darwin because `syscall.SIGCLD` does not
    exist.
    
    Signed-off-by: David Scott <dave@recoil.org>

--- pkg/signal/signal_linux_test.go
+++ pkg/signal/signal_linux_test.go
@@ -1,5 +1,3 @@
-// +build darwin linux
-
 package signal // import "github.com/docker/docker/pkg/signal"
 
 import (
@@ -33,33 +31,4 @@ func TestCatchAll(t *testing.T) {
 			assert.Check(t, is.Equal(s.String(), signal.String()))
 		}
 	}
-}
-
-func TestCatchAllIgnoreSigUrg(t *testing.T) {
-	sigs := make(chan os.Signal, 1)
-	CatchAll(sigs)
-	defer StopCatch(sigs)
-
-	err := syscall.Kill(syscall.Getpid(), syscall.SIGURG)
-	assert.NilError(t, err)
-	timer := time.NewTimer(1 * time.Second)
-	defer timer.Stop()
-	select {
-	case <-timer.C:
-	case s := <-sigs:
-		t.Fatalf("expected no signals to be handled, but received %q", s.String())
-	}
-}
-
-func TestStopCatch(t *testing.T) {
-	signal := SignalMap["HUP"]
-	channel := make(chan os.Signal, 1)
-	CatchAll(channel)
-	syscall.Kill(syscall.Getpid(), signal)
-	signalString := <-channel
-	assert.Check(t, is.Equal(signalString.String(), signal.String()))
-
-	StopCatch(channel)
-	_, ok := <-channel
-	assert.Check(t, is.Equal(ok, false))
 }
